452da8b2bfad8804025b622b3db49b56d575ed04
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-07-22 22:43:24 -0700

    added player

--- blackjack.rb
+++ blackjack.rb
@@ -1,1 +1,23 @@
+
+class Player
+  def initialize(name_from_input)
+    @name = name_from_input
+  end
+
+  def name
+    @name
+  end
+end
+
+
+
+
+
 puts "WELCOME TO BLACKJACK!"
+
+puts "what is your name?: "
+input = gets
+player = Player.new(input)
+puts "welcome, #{player.name}"
+
+
