446a9139c8944f01c7e9f3aa19fa4326ac924c33
Maxwell Salzberg <maxwell@joindiaspora.com>
2012-07-22 23:17:39 -0700

    Added Deck

--- blackjack.rb
+++ blackjack.rb
@@ -9,7 +9,29 @@ class Player
   end
 end
 
+class Deck
 
+  SUITS = ["H", "S", "C", "D"]
+  FACE_VALUE = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+  def initialize
+    @cards = Deck.construct_cards
+  end
+
+  def cards
+    @cards
+  end
+
+  def self.construct_cards
+    cards = []
+    SUITS.each do |suit|
+      FACE_VALUE.each do |face_value|
+        cards << face_value + suit
+      end
+    end
+    cards
+  end
+end
 
 
 
@@ -19,5 +41,6 @@ puts "what is your name?: "
 input = gets
 player = Player.new(input)
 puts "welcome, #{player.name}"
+deck = Deck.new
 
 
